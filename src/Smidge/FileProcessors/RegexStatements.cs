615bc6bedbcbd6c450e90ead4319de278085d56a
Shannon <sdeminick@gmail.com>
2015-05-20 11:48:40 +1000

    Adds CssUrlProcessorTests and fixes up the regex for svg url

--- src/Smidge/FileProcessors/RegexStatements.cs
+++ src/Smidge/FileProcessors/RegexStatements.cs
@@ -6,6 +6,6 @@ namespace Smidge.FileProcessors
     internal class RegexStatements
     {
         public static readonly Regex ImportCssRegex = new Regex(@"@import (url\((.+?)\)|[""'](.+?)[""']);?", RegexOptions.Compiled);
-        public static readonly Regex CssUrlRegex = new Regex(@"url\(((?![""']?data:).+?)\)", RegexOptions.Compiled);
+        public static readonly Regex CssUrlRegex = new Regex(@"url\(((?![""']?data:|[""']?#).+?)\)", RegexOptions.Compiled);
     }
 }
