5fb11c4c833b8a822b537c91a09bee021eef5d26
Shannon <sdeminick@gmail.com>
2015-11-27 09:39:21 +0100

    adds test ps script

---
+++ tests.ps1
@@ -0,0 +1,22 @@
+$PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
+
+" PSScriptFilePath = $PSScriptFilePath"
+
+$SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
+$TestsFolder = Join-Path -Path $SolutionRoot -ChildPath "tests/Smidge.Tests";
+
+Set-Location -Path $TestsFolder
+
+$DNX = "dnx"
+$DNVM = "dnvm"
+
+# use the correct version
+& $DNVM use 1.0.0-rc1-final
+
+# run them
+& $DNX test
+
+if (-not $?)
+{
+	throw "Tests failed"
+}
