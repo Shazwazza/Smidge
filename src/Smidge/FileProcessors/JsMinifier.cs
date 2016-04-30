d0b3d2ebd1c4053bab56854144f20935fcf170cd
Shannon <sdeminick@gmail.com>
2016-04-30 20:09:19 +0200

    updates jsmin

--- src/Smidge/FileProcessors/JsMinifier.cs
+++ src/Smidge/FileProcessors/JsMinifier.cs
@@ -194,21 +194,22 @@ namespace Smidge.FileProcessors
                         _start = true;
 
                         //process unary operator
-                        var handled1 = HandleUnaryOperator();
+                        HandleUnaryOperator();
 
                         goto case 2;
                     case 2:
                         _theA = _theB;
 
                         //process string literals or end of statement and track return statement
-                        var handled2 = HandleStringLiteral() || HandleEndOfStatement();
+                        if (!HandleStringLiteral())
+                            HandleEndOfStatement();
 
                         goto case 3;
                     case 3:
                         _theB = NextCharExcludingComments();
 
                         //track return statement
-                        var handled3 = TrackReturnStatement();
+                        TrackReturnStatement();
 
                         //Check for a regex literal and process it if it is found
                         HandleRegexLiteral();
@@ -418,12 +419,14 @@ namespace Smidge.FileProcessors
                 // for example see these bug reports: 
                 //  https://github.com/douglascrockford/JSMin/issues/11
                 //  https://github.com/Shazwazza/ClientDependency/issues/73                    
-
+                //  https://github.com/Shazwazza/JsMinSharp/issues/8
                 //The original logic from JSMin doesn't cater for the above issues mentioned
                 // We've now added these additional characters to be able to preceed a regex literal: +
                 // And now we also track a return statement which can preceed a regex literal.
-                const string toMatch = "(,=:[!&|?+-~*/{\n+";
-                if (toMatch.IndexOf((char)_theA) < 0 && _retStatement != 5)
+                // To fix the single line no-op  issue - we need to allow for a new line to precede a 
+                // regex statement too but _theA will not be a newline char here, only _theY will be.
+                const string toMatch = "(,=:[!&|?+-~*/{\n+;";
+                if (toMatch.IndexOf((char)_theA) < 0 && (char)_theY != '\n' && _retStatement != 5)
                     return false;
 
                 Put(_theA);
