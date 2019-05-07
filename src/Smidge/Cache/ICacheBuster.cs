319d0a43b865448220ad821a4e329a1801db09dc
Shannon <sdeminick@gmail.com>
2019-05-08 01:28:41 +1000

    Adds notes

--- src/Smidge/Cache/ICacheBuster.cs
+++ src/Smidge/Cache/ICacheBuster.cs
@@ -27,6 +27,13 @@ namespace Smidge.Cache
         /// </remarks>
         string GetValue();
 
+
+        //TODO: This doesn't do anything!! In order for this setting to have any affect a pretty big overhaul of everything has to take place.
+        // The problem is that Smidge relies heavily on a file system cache. For example, the PreProcessManager specifically writes the output
+        // of processing to cache files, not returning the result so we can't not have a cache. In theory perhaps the 'easiest' way is to remove
+        // this property and add a virtual cache, one that is a physical disk, one that is memory, etc... and one that is Request based. But this 
+        // is still ugly! Since there would be not 'pass through' type. I think the whole processing needs to be re-designed.
+
         /// <summary>
         /// Returns true if the server will persist the processed files to disk so that the processing survives app restarts
         /// </summary>
---
+++ src/Smidge/Cache/NoLifetimeCacheBuster.cs
@@ -0,0 +1,24 @@
+﻿//TODO: This cannot work! This is due to the behavior of Smidge relying so heavily on cached files and not input/output.
+// this keeps getting called over and over to re-resolve a file path but this will change every time and not be consistent
+// within the request which it would need to be. 
+
+//using System;
+
+//namespace Smidge.Cache
+//{
+//    /// <summary>
+//    /// Cache bust on every request
+//    /// </summary>
+//    public class NoLifetimeCacheBuster : ICacheBuster
+//    {
+//        public string GetValue()
+//        {
+//            return DateTime.UtcNow.Ticks.ToString();
+//        }
+
+//        /// <summary>
+//        /// Since the cache will be busted on every restart we don't want to persist the files to the server, they will just be stored in memory
+//        /// </summary>
+//        public bool PersistProcessedFiles => false;
+//    }
+//}
