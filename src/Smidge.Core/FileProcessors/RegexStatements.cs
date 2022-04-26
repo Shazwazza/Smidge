959e85985bd66fae1d333b258762be9726232f50
PhyxionNL <7643972+PhyxionNL@users.noreply.github.com>
2022-04-26 15:01:19 +0200

    Fixes #151

--- src/Smidge.Core/FileProcessors/RegexStatements.cs
+++ src/Smidge.Core/FileProcessors/RegexStatements.cs
@@ -6,7 +6,7 @@ namespace Smidge.FileProcessors
     public class RegexStatements
     {
         public static readonly Regex ImportCssRegex = new Regex(@"^\s*@import (url\((.+?)\)|[""'](.+?)[""'])\s*;?", RegexOptions.Compiled | RegexOptions.Multiline);
-        public static readonly Regex CssUrlRegex = new Regex(@"url\(((?![""']?data:|[""']?#).+?)\)", RegexOptions.Compiled);
+        public static readonly Regex CssUrlRegex = new Regex(@"url\(((?![""']?data:|[""']?#|[""']?%23).+?)\)", RegexOptions.Compiled);
         public static readonly Regex SourceMap = new Regex(@"^\s*//# sourceMappingURL=(.+?)(?:;|\s|$)", RegexOptions.Compiled);
     }
 }
