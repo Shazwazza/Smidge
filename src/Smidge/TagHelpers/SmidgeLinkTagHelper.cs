b1764f2c870a1ff9e01914a1cf8b2d3ab6a45055
Greg White <greg@superloans.co.nz>
2022-08-21 13:01:48 +1200

    Updated TagHelpers to pass a null Debug parameter to SmidgeHelper by default so the ISmidgeProfileStrategy is used to determine which profile to use. The Debug parameter can still be used to override the output on a case-by-case basis.

--- src/Smidge/TagHelpers/SmidgeLinkTagHelper.cs
+++ src/Smidge/TagHelpers/SmidgeLinkTagHelper.cs
@@ -61,8 +61,12 @@ namespace Smidge.TagHelpers
         [HtmlAttributeName(HrefAttributeName)]
         public string Source { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to generate content based on the debug or production configuration profile.
+        /// If left unset then the configured <see cref="ISmidgeProfileStrategy"/> will determine if the debug profile is used.
+        /// </summary>
         [HtmlAttributeName("debug")]
-        public bool Debug { get; set; }
+        public bool? Debug { get; set; }
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
